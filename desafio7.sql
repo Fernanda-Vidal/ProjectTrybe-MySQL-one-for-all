21635abdfadbe79986e0f74784133a918654c3b9
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-18 16:33:11 -0300

    Add req 7.

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+	artist.artist_name AS artista,
+    albums.title_album AS album,
+    COUNT(follow.artist_id) AS seguidores
+FROM SpotifyClone.albums AS albums
+INNER JOIN SpotifyClone.artists AS artist
+INNER JOIN SpotifyClone.use_art AS follow
+	ON albums.artist_id = artist.artist_id AND artist.artist_id = follow.artist_id
+GROUP BY albums.album_id
+ORDER BY seguidores DESC, artista, album;
+
